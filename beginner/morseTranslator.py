9a516f03d01eed48aef262b239d874a533c51c59
Cartha007 <g98kattah@gmail.com>
2023-09-06 16:58:14 +0000

    Error handling

--- beginner/morseTranslator.py
+++ beginner/morseTranslator.py
@@ -14,12 +14,23 @@ def morse_to_english(text):
     pass
 
 def options():
-    print('(1) English to Morse code.\n(2) Morse code to English.')
+    print('(1) English to Morse code.\n(2) Morse code to English.\n(3) Quit.')
 
 def main():
     while True:
         options()
-        response = input('> ')
+        response = int(input('> '))
+        if response == 1:
+            text = input("Enter text to convert to morse: ")
+            morseCode = english_to_morse(text)
+            print(f"Morse code: {morseCode}")
+        elif response == 2:
+            morseCode = input("Enter Morse code to convert to English: ")
+            text = morse_to_english(morseCode)
+            print(f"English: {text}")
+        else:
+            print('Quitting...')
+            break
 
 
 if __name__ == "__main__":
@@ -27,3 +38,7 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("\nEnding script...")
+    except ValueError:
+        print("Error: Invalid character!")
+    except Exception as e:
+        print(f'Error: {e}')
