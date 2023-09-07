2e7ac1b596d0dc4ed312df22ddb02939d4b9c2cc
Cartha007 <g98kattah@gmail.com>
2023-09-07 19:04:03 +0000

    some changes

--- intermediate/currencyConvertor.py
+++ intermediate/currencyConvertor.py
@@ -1,10 +1,19 @@
 import requests
 
+'''
+You would need an open exchange rates account for an app_id
+'''
+
 def convert_currency():
     pass
 
 def main():
-    pass
+    app_id = "Your_App_Id"
+    
+    url = f"https://openexchangerates.org/api/latest.json?app_id={app_id}"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
 
 if __name__ == "__main__":
     main()
