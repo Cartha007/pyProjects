0bfed527193c299b38d56eafe36592148df23fd6
Cartha007 <g98kattah@gmail.com>
2023-09-08 11:13:51 +0000

    optimised and made changes

--- intermediate/currencyConvertor.py
+++ intermediate/currencyConvertor.py
@@ -6,9 +6,6 @@ This is just a CLI based app, GUI to be added soon.
 You would need an open exchange rates account for an app_id
 '''
 
-def convert_currency(baseCurrency, amount, currency2):
-    pass
-
 def availableCurrencies():
     url = "https://docs.openexchangerates.org/reference/supported-currencies"
     page = requests.get(url).text
@@ -22,13 +19,12 @@ def availableCurrencies():
     # Extract table rows and data
     row_data = [[td.text.strip() for td in row.find_all('td')] for row in table.find_all('tr')]
     
-    return [headings, row_data]
+    return headings, row_data
 
 def listCurrencies():
     data = availableCurrencies()
     # Print the table headers and data
-    headings = data[0]
-    row_data = data[1]
+    headings, row_data = availableCurrencies()
     
     print('\t'.join(headings))
     
@@ -37,8 +33,13 @@ def listCurrencies():
     print() # Newline
 
 def isCurrencyAvailable(baseCurrency):
-    currencies = availableCurrencies()
-    return baseCurrency in [x[0] for x in currencies[1] if x]
+    _, currencies = availableCurrencies()
+    return baseCurrency in [x[0] for x in currencies if x]
+
+def getCurrencyName(currency):
+    _, currencies = availableCurrencies()
+    name = [x[1] for x in currencies if x and x[0] == currency][0]
+    return name if name else "Unknown name"
 
 def getCurrencies():
     return
@@ -48,6 +49,40 @@ def getCurrencies():
     response = requests.get(url)
     data = response.json()
     print(data)
+
+def convert_currency(baseCurrency, amount, currency2):
+    if baseCurrency == currency2:
+        return amount
+    pass
+
+def convert_currency_prompt():
+    baseCurrency = input("Enter a base currency: ").upper()
+    if isCurrencyAvailable(baseCurrency):
+        amount = float(input(f"Enter amount in {baseCurrency}: "))
+        currency2 = input("Enter a currency to convert to: ").upper()
+        if isCurrencyAvailable(currency2):
+            name = getCurrencyName(currency2)
+            result = convert_currency(baseCurrency, amount, currency2)
+            print(f"{amount} {baseCurrency} in {currency2}({name}) is {result}")
+        else:
+            print("Currency not available.")
+    else:
+        print("Currency not available.")
+        
+def getExchangeRate(currency1, currency2):
+    rate = None # Placeholder for exchange rate
+    print(f'Exchange rate for 1 {currency1} in {currency2} is {rate}')
+
+def getExchangeRate_prompt():
+    currency1 = input("Enter currency 1: ").upper()
+    if isCurrencyAvailable(currency1):
+        currency2 = input("Enter currency 2: ").upper()
+        if isCurrencyAvailable(currency2):
+            getExchangeRate(currency1, currency2)
+        else:
+            print("Currency not available.")
+    else:
+        print("Currency not available.")
 
 def options():
     print("Select an option:")
@@ -62,7 +97,7 @@ def banner():
     print("╚════════════════════════╝")
 
 def main():
-    # currencies = getCurrencies()
+    banner()
     while True:
         try:
             options()
@@ -71,28 +106,20 @@ def main():
                 print('Please wait, fetching available currencies...\n')
                 listCurrencies()
             elif response == 2:
-                baseCurrency = input("Enter a base currency: ").upper()
-                if isCurrencyAvailable(baseCurrency):
-                    amount = float(input(f"Enter amount in {baseCurrency}: "))
-                    currency2 = input("Enter a currency to convert to: ").upper()
-                    result = convert_currency(baseCurrency, amount, currency2)
-                    print(f"{amount} {baseCurrency} in {currency2} is {result}")
-                else:
-                    print("Currency not available.")
+                convert_currency_prompt()
             elif response == 3:
-                pass
+                getExchangeRate_prompt()
             elif response == 4:
                 print("Quitting..")
                 break
             else:
                 print("Invalid option.")
+        except KeyboardInterrupt:
+            print("\nEnding script...")
+            break
         except ValueError:
-            print("Invalid!")
+            print("Invalid input! Enter a valid option.")
 
 
 if __name__ == "__main__":
-    try:
-        banner()
-        main()
-    except KeyboardInterrupt:
-        print("\nEnding script...")
+    main()
