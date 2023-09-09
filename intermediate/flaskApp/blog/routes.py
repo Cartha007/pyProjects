fa204a850998ee0e24b57ffe051b886c0bfe105f
Cartha007 <g98kattah@gmail.com>
2023-09-09 23:27:48 +0000

    Added navbar and error pages

--- intermediate/flaskApp/blog/routes.py
+++ intermediate/flaskApp/blog/routes.py
@@ -5,3 +5,15 @@ from flask import render_template, redirect, url_for, flash, request
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+# Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
