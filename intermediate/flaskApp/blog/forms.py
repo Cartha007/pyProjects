2afb49c6059312406c523ec130f33c02bc409d5d
Cartha007 <g98kattah@gmail.com>
2023-09-24 13:09:12 +0000

    Starting the blog project

--- intermediate/flaskApp/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
-class NamerForm(FlaskForm):
-    name = StringField(label="What's Your Name", validators=[DataRequired()])
-    submit = SubmitField("Submit")
