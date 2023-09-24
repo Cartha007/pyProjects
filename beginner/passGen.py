2e7b363f638617c364f3177e1e4e90609945aead
Cartha007 <g98kattah@gmail.com>
2023-09-24 12:56:03 +0000

    add password generator

--- beginner/passGen.py
+++ beginner/passGen.py
@@ -2,21 +2,20 @@ import random
 import string
 
 def generate_password(length, add_upper, add_lower, add_numbers, add_special_char):
-    character_set = []
+    characters = ""
     if add_upper:
-        character_set.append(string.ascii_uppercase)
+        characters += string.ascii_uppercase
     if add_lower:
-        character_set.append(string.ascii_lowercase)
+        characters += string.ascii_lowercase
     if add_numbers:
-        character_set.append(string.digits)
+        characters += string.digits
     if add_special_char:
-        character_set.append(string.punctuation)
+        characters += string.punctuation
     
-    if not character_set:
+    if not characters:
         print("Please select at least one option.")
         return None
-    characters = "".join(random.choice(character_set))
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = "".join(random.choice(characters) for _ in range(length))
     return password
 
 def main():    
