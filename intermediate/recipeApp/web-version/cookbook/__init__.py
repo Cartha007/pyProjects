c12227381a456ebf23f83b7fc00087f6003ed8d7
Cartha007 <g98kattah@gmail.com>
2023-10-01 20:14:20 +0000

    Added basic structure

---
+++ intermediate/recipeApp/web-version/cookbook/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+# App configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'secret_key'
+
+# Database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#  For Encryption
+bcrypt = Bcrypt(app)
+
+# Login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+# For file upload
+UPLOAD_FOLDER = 'blog/static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+from cookbook import routes
