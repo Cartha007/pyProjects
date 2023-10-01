6d15eab5289cc37e68cbc5011c2e65d23786dc0f
Cartha007 <g98kattah@gmail.com>
2023-10-01 19:32:02 +0000

    implemented the logic for viewing and editing a recipe and made other changes

--- intermediate/recipeApp/CLI-version/main.py
+++ intermediate/recipeApp/CLI-version/main.py
@@ -10,6 +10,26 @@ def load_recipes():
     except FileNotFoundError:
         return []
 
+def view_recipe(recipes_data):
+    if not recipes_data:
+        print("\nYour recipe book is empty.")
+        return
+    
+    print("\nSelect a recipe to view:")
+    for idx, recipe in enumerate(recipes_data, start=1):
+        print(f"{idx}. {recipe.name}")
+    
+    try:
+        choice = int(input("Enter the number of the recipe to view: "))
+        
+        if 1 <= choice <= len(recipes_data):
+            selected_recipe = recipes_data[choice - 1]
+            selected_recipe.display_recipe()
+        else:
+            print("Invalid choice. Please enter a valid number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
 def add_recipe(recipes_data):
     recipe_name = input("Enter the name of the recipe: ")
     ingredients = input("Enter the ingredients (comma-separated): ").split(",")
@@ -24,10 +44,41 @@ def add_recipe(recipes_data):
     new_recipe = Recipe(recipe_name.strip(), [ingredient.strip() for ingredient in ingredients], instructions)
     recipes_data.append(new_recipe)
     print(f"{recipe_name} has been added to your recipe book.")
+    
+def edit_recipe(recipes_data):
+    if not recipes_data:
+        print("Your recipe book is empty.")
+        return
+    
+    print("Select a recipe to edit:")
+    for idx, recipe in enumerate(recipes_data, start=1):
+        print(f"{idx}. {recipe.name}")
+    
+    try:
+        choice = int(input("Enter the number of the recipe to edit: "))
+        
+        if 1 <= choice <= len(recipes_data):
+            selected_recipe = recipes_data[choice - 1]
+            
+            # Prompt the user for new information and update the recipe
+            name = input(f"Enter the new name for {selected_recipe.name}: ")
+            ingredients = input(f"Enter the new ingredients (comma-separated) for {selected_recipe.name}: ").split(",")
+            instructions = []
+            print(f"Enter the new instructions for {selected_recipe.name} (one per line, press Enter to finish):")
+            while True:
+                instruction = input()
+                if not instruction:
+                    break
+                instructions.append(instruction)
+            
+            selected_recipe.edit_recipe(name.strip(), [ingredient.strip() for ingredient in ingredients], instructions)
+            print(f"{name} has been updated.")
+        else:
+            print("Invalid choice. Please enter a valid number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
 def delete_recipe(recipes_data):
-    print("\nDelete Recipe")
-    
     if not recipes_data:
         print("Your recipe book is empty.")
         return
@@ -41,7 +92,7 @@ def delete_recipe(recipes_data):
         
         if 1 <= choice <= len(recipes_data):
             deleted_recipe = recipes_data.pop(choice - 1)
-            print(f"{deleted_recipe.name} has been deleted from your recipe book.")
+            print(f"\033[1;31m{deleted_recipe.name} has been deleted from your recipe book.\033[0m")
         else:
             print("Invalid choice. Please enter a valid number.")
     except ValueError:
@@ -52,6 +103,7 @@ def save_recipes(recipes):
     with open('recipes.json', 'w') as file:
         recipes_data = [{"name": recipe.name, "ingredients": recipe.ingredients, "instructions": recipe.instructions} for recipe in recipes]
         json.dump(recipes_data, file, indent=4)
+    print("\033[1;32mSaved successfully!\033[0m")
 
 def exit_program(recipes_data):
     print("1. Save and exit\n2. Exit without saving")
@@ -68,10 +120,10 @@ def exit_program(recipes_data):
 def main():
     recipes_data = load_recipes()
     title = "Cook's HandBook"
-    print(f"\n\x1b[1;33;44m{title}\x1b[0m\n")
+    print(f"\n\x1b[1;33;44m{title}\x1b[0m")
     
     while True:
-        print("1. Add Recipe")
+        print("\n1. Add Recipe")
         print("2. View Recipe")
         print("3. Edit Recipe")
         print("4. Delete Recipe")
@@ -83,11 +135,9 @@ def main():
             if choice == 1:
                 add_recipe(recipes_data)
             elif choice == 2:
-                pass
-                # Implement logic to view a recipe
+                view_recipe(recipes_data)
             elif choice == 3:
-                pass
-                # Implement logic to edit a recipe
+                edit_recipe(recipes_data)
             elif choice == 4:
                 delete_recipe(recipes_data)
             elif choice == 5:
--- intermediate/recipeApp/CLI-version/recipe.py
+++ intermediate/recipeApp/CLI-version/recipe.py
@@ -3,21 +3,21 @@ class Recipe:
         self.name = name
         self.ingredients = ingredients
         self.instructions = instructions
-        
+        # \033[1;36mCurrency Converter\033[0m
     def display_recipe(self):
-        print(f'Recipe Name: {self.name}')
+        print(f'\n\033[1;36mRecipe Name\033[0m: {self.name}')
         if self.ingredients:
-            print('Ingredients:')
+            print('\n\033[1;36mIngredients\033[0m:')
             for ingredient in self.ingredients:
                 print(f"-> {ingredient}")
         else:
-            print("Ingredients unknown.")
+            print("\n\033[1;31mIngredients unknown.\033[0m")
         if self.instructions:
-            print("Instructions:")
+            print("\n\033[1;36mInstructions\033[0m:")
             for i, instruction in enumerate(self.instructions, start=1):
                 print(f"{i}. {instruction}")
         else:
-            print("No instructions available.")
+            print("\n\033[1;31mNo instructions available.\033[0m")
             
     def edit_recipe(self, name, ingredients, instructions):
         self.name = name
