af40f1a920405dea506171f80e3e0c3f9117f4fd
Cartha007 <g98kattah@gmail.com>
2023-10-02 20:36:30 +0000

    Made some changes

--- intermediate/recipeApp/web-version/cookbook/__init__.py
+++ intermediate/recipeApp/web-version/cookbook/__init__.py
@@ -18,10 +18,10 @@ migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 
 # Login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login_page"
-login_manager.login_message_category = "info"
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = "login_page"
+# login_manager.login_message_category = "info"
 
 # For file upload
 UPLOAD_FOLDER = 'blog/static/images/'
---
+++ intermediate/recipeApp/web-version/cookbook/routes.py
@@ -0,0 +1,18 @@
+from cookbook import app
+# from cookbook.models import User
+from flask import render_template, redirect, url_for, flash, request
+# from cookbook.models import User, Posts
+# from cookbook.forms import RegisterForm, LoginForm, SearchForm
+from cookbook import db
+from sqlalchemy import desc
+# from flask_login import login_user, logout_user, login_required, current_user
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.context_processor
+def base():
+    form = None
+    return dict(form=form)
