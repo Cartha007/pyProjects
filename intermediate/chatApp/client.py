2708cffece18c7c16908e021f7ead9c938973c31
Cartha007 <g98kattah@gmail.com>
2023-10-14 19:31:24 +0000

    Add chatapp

---
+++ intermediate/chatApp/client.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket()
+host = input(str("Please enter the hostname of the server: "))
+port = 8080
+s.connect((host,port))
+print("Connected to chat server.")
+
+while True:
+    incoming_msg = s.recv(1024)
+    incoming_msg = incoming_msg.decode()
+    print(f"Server: {incoming_msg}")
+    print("Enter your message")
+    message = input(str(">"))
+    message = message.encode()
+    s.send(message)
+    print("Message has been sent...")
---
+++ intermediate/chatApp/server.py
@@ -0,0 +1,24 @@
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+print(f'Server: {host}')
+
+port = 8080
+s.bind((host, port))
+print("\nServer done binding to host and port successfully.")
+print("Server is waiting for incoming connections")
+s.listen(1)
+conn, addr = s.accept()
+print(f"{addr} Has connected to the server and is now online...")
+
+while True:
+    print("Enter your message")
+    message = input(str(">"))
+    message = message.encode()
+    conn.send(message)
+    print("Message has been sent...")
+    incoming_msg = conn.recv(1024)
+    incoming_msg = incoming_msg.decode()
+    print(f"Client: {incoming_msg}")
