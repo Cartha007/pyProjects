15b25c56d7559965a4e28f860e26dac20a8eac58
Cartha007 <g98kattah@gmail.com>
2023-10-24 16:15:09 +0000

    Made some changes

--- advance/alarm clock/alarm.py
+++ advance/alarm clock/alarm.py
@@ -1,3 +1,35 @@
 import time
+import datetime
+import winsound
 
-print(time.strftime("%a %A %b %B %c %d %H %I %j %m %M %p %S %U %w %W %x %X %y %Y %z %Z %%"))
+
+
+def countdown(seconds):
+    time_elapsed = 0
+    
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+        
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+        
+        print(f"{minutes_left:02}:{seconds_left:02}")
+
+def alarm():
+    hour = int(input("Enter hour: "))
+    minute = int(input("Enter minutes: "))
+    alarmAm = input("am / pm: ").lower()
+    
+    if alarmAm == "pm":
+        hour += 12
+        
+    while True:
+        if hour == datetime.datetime.now().hour and minute == datetime.datetime.now().minute:
+            print("Time up!")
+            for i in range(20):
+                winsound.Beep(1000, 1000)
+            break
+
+alarm()
